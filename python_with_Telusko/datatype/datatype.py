c01377a6b14af79a418d126438fe1468236b3b49
Emmanuel Tar <emmanueltar14@gmail.com>
2023-08-04 09:27:48 +0100

    python_with_Telusko Tutorials on Youtube with Navin Reddy

---
+++ python_with_Telusko/datatype/datatype.py
@@ -0,0 +1,8 @@
+num = 2.5
+print(type(num))
+# ---------------------------
+# float() function to convert a string into floating point number:
+#int():
+#complex:
+#boolen:<jupyter_output><empty_output><jupyter_text>**String Operations:**<jupyter_code>a="Hello" #string
+#----------------------------------------

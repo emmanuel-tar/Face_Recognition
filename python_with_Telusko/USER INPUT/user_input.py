bfe02bf9163e085003de50b72fc3891ec17c10cb
Emmanuel Tar <emmanueltar14@gmail.com>
2023-08-12 09:46:33 +0100

    Dictionary/Mapping using functions such as values,get and etc to manipulate the map

--- python_with_Telusko/USER INPUT/user_input.py
+++ python_with_Telusko/USER INPUT/user_input.py
@@ -1,4 +1,4 @@
-ch = input('Enter Your Full name: ')[0]
+ch = input('Enter Your Full name: ')[0:5]
 # receiving input from user and printing out character
 # as they are stored in an array of characters
 print(ch)
@@ -8,3 +8,7 @@ print(ch)
 # from a user input
 ex = eval(input("enter an expression"))
 print(ex)
+
+num = int(input('enter any number: '))
+cube = num **3
+print(cube)

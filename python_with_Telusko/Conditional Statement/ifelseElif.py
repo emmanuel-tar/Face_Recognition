e86832c47273385a7294574585c55ec32dc09f83
Emmanuel Tar <emmanueltar14@gmail.com>
2023-08-12 10:12:48 +0100

    Dictionary/Mapping using functions such as values,get and etc to manipulate the map

---
+++ python_with_Telusko/Conditional Statement/ifelseElif.py
@@ -0,0 +1,26 @@
+# x=8
+# r= x % 2
+# if r==0:
+#     print('Even Number')
+#
+# else:
+#     print('Odd Number')
+#
+#
+#
+# print('bye')
+x=int(input("Enter any number from 1 -  4: "))
+
+if x==1:
+    print('One')
+elif x==2:
+    print('Two')
+
+elif x==3:
+    print('Three')
+
+elif x==4:
+    print('Four')
+
+else:
+    print('Wrong Input')

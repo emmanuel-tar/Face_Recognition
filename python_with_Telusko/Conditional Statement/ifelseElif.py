1a51f485bacdfc5c02d25ac54c218a70e288be3f
Emmanuel Tar <emmanueltar14@gmail.com>
2023-08-12 10:51:32 +0100

    Dictionary/Mapping using functions such as values,get and etc to manipulate the map

--- python_with_Telusko/Conditional Statement/ifelseElif.py
+++ python_with_Telusko/Conditional Statement/ifelseElif.py
@@ -9,18 +9,53 @@
 #
 #
 # print('bye')
-x=int(input("Enter any number from 1 -  4: "))
+# x=int(input("Enter any number from 1 -  4: "))
 
-if x==1:
-    print('One')
-elif x==2:
-    print('Two')
+# if x==1:
+#     print('One')
+# elif x==2:
+#     print('Two')
+#
+# elif x==3:
+#     print('Three')
+#
+# elif x==4:
+#     print('Four')
+#
+# else:
+#     print('Wrong Input')
 
-elif x==3:
-    print('Three')
+    #1. write a code to check given
+    # number is positive or negative
 
-elif x==4:
-    print('Four')
+    # 2. take three values from user and
+    # find the greatest number from them
 
+    # solution
+
+# num = int(input('Enter any number: '))
+# if num >=0:
+#
+#     if num == 0:
+#         print('Zero')
+#     else:
+#         print('Positive Number')
+# else:
+#     print('Negative Number')
+
+
+
+# Solution#2
+
+num1 = int(input('Enter First Number: '))
+num2 = int(input('Enter Second Number: '))
+num3 = int(input('Enter Third Number: '))
+
+if(num1 >=num2) and (num1>=num3):
+    largest =num1
+elif(num2>=num1) and (num2>=num3):
+    largest = num2
 else:
-    print('Wrong Input')
+    largest =num3
+
+print('The largest value is: ', largest)
